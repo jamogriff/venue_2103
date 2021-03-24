baf8c7e59e2b25990b68897c8fd71a034a6f67aa
Jamison Griffith <jamison.griffith@gmail.com>
2021-03-24 14:40:53 -0600

    Tests pass for iteration 1 and 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(venue_name, venue_capacity)
+    @patrons = []
+    @name = venue_name
+    @capacity = venue_capacity
+  end
+
+  def add_patron(patron_name)
+    @patrons << patron_name
+  end
+
+  def yell_at_patrons
+    capitalized_names = []
+    @patrons.each do |patron|
+      capitalized_names << patron.upcase
+    end
+    return capitalized_names
+  end
 end
